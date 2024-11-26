757d603236eafe6eceefdd166117036ee3ba81be
RajAditya11 <rajadi7185@gmail.com>
2024-11-26 07:38:53 +0530

    Number is game is completed.

--- Guess the Number/backend.js
+++ Guess the Number/backend.js
@@ -1,34 +1,34 @@
 let randomNumber = 0;
 let Lives = 0;
+let start = null;
+let end = null;
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example usage
-const randomInt = getRandomInteger(5, 10); // Generates a random integer between 5 and 10 (inclusive)
-console.log(randomInt);
-
-
 document.querySelector('#getrange-btn').addEventListener('click', () => {
-    const start = document.querySelector('#start-value').value;
-    const end = document.querySelector('#end-value').value;
+    const startValue = document.querySelector('#start-value').value;
+    const endValue = document.querySelector('#end-value').value;
 
-    if (!isNaN(start) && !isNaN(end) && start.trim() !== '' && end.trim() !== '') {
-        console.log(start, end);
+    start = startValue;
+    end = endValue;
+
+    if (!isNaN(startValue) && !isNaN(endValue) && startValue.trim() !== '' && endValue.trim() !== '') {
+        console.log(startValue, endValue);
         document.querySelector('.rangetext').innerHTML = `We have it now! Oh its raaaaaange`;
-        document.querySelector('#play-text').innerHTML = `What do you think it is?`;
+        document.querySelector('#play-text').innerHTML = `Tell the random number?`;
 
-        let totalNumbers = end - start;
+        let totalNumbers = endValue - startValue;
         let totalMoves = Math.ceil(totalNumbers / 3);
 
         document.querySelector('#total-life').innerHTML = `${totalMoves}`;
         Lives = totalMoves;
 
-        let random = getRandomInteger(Number(start), Number(end));
+        let random = getRandomInteger(Number(startValue), Number(endValue));
         randomNumber = random;
-        console.log(randomNumber, random);
     } else {
+        reset();
         alert("Please enter valid range bruh!");
     }
 });
@@ -37,7 +37,11 @@ document.querySelector('#getrange-btn').addEventListener('click', () => {
 
 document.querySelector('#submit').addEventListener('click', () => {
     const userinput = document.querySelector('#user-input').value;
-
+    if( isNaN(start) || isNaN(end)){
+        alert("Range your game.");
+    }else if( isNaN(userinput) || userinput.trim() == ''){
+        alert("Choose your randome number.");
+    }
     if (!isNaN(userinput) && userinput.trim() !== '') {
         if (Number(userinput) === randomNumber) {
             alert('Winner !');
@@ -50,8 +54,6 @@ document.querySelector('#submit').addEventListener('click', () => {
                 reset();
             }
         }
-    } else {
-        alert("Enter start and end point first!");
     }
 });
 
